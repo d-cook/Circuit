adfde09c3a1d7b344c5ad90308d8073035dc864a
dcook <dcook0819@gmail.com>
2019-10-13 10:17:35 -0700

    Create gate constructors

--- Circuit.js
+++ Circuit.js
@@ -20,82 +20,154 @@ let numInputs = 2;
 let gateSize = 30;
 let mouse = { x: 0, y: 0 };
 
-function renderAnd(x, y, not, inputs) {
-    let len = inputs.length;
-    let inputGap = Math.max(spacing, Math.floor(gateSize / (len + 0)));
-    let inputTop = y + Math.floor((gateSize - inputGap * (len - 1)) / 2);
-    let inputBottom = y + Math.floor((gateSize + inputGap * (len - 1)) / 2);
-    return [
-        ['closed path',
-            x + stubLen, y,
-            [x + gateSize/2, y],
-            [x + gateSize, y, x + gateSize, y + gateSize/2],
-            [x + gateSize, y + gateSize, x + gateSize/2, y + gateSize],
-            [x + stubLen, y + gateSize]
-        ],
-        ['line', x + stubLen, inputTop, x + stubLen, inputBottom],
-        (not ? ['circle', x + gateSize + stubLen/2, y + gateSize/2, stubLen/2]
-             : ['line', x + gateSize, y + gateSize/2, x + gateSize + stubLen, y + gateSize/2]
-         )
-    ].concat(inputs.map((v, i) =>
-        ['line', x, inputTop + i*inputGap, x + stubLen, inputTop + i*inputGap]
-    ));
+function Value(val) {
+    return {
+        state    : (typeof value === 'function') ? val : () => !!val,
+        propagate: () => { },
+        update   : () => { }
+    };
 }
 
-function renderOr(x, y, not, inputs) {
-    let len = inputs.length;
-    let inputGap = Math.max(spacing, Math.floor(gateSize / (len + 0)));
-    let inputTop = y + Math.floor((gateSize - inputGap * (len - 1)) / 2);
-    let inputBottom = y + Math.floor((gateSize + inputGap * (len - 1)) / 2);
-    return [
-        ['path',
-            x + stubLen, y,
-            [x + gateSize*3/4, y - gateSize/10, x + gateSize, y + gateSize/2],
-            [x + gateSize*3/4, y + gateSize*11/10, x + stubLen, y + gateSize],
-            [x + stubLen*2, y + gateSize*2/3, x + stubLen*2, y + gateSize/3, x + stubLen, y]
-        ],
-        ['line', x + stubLen, inputTop, x + stubLen, inputBottom],
-        (not ? ['circle', x + gateSize + stubLen/2, y + gateSize/2, stubLen/2]
-             : ['line', x + gateSize, y + gateSize/2, x + gateSize + stubLen, y + gateSize/2]
-         )
-    ].concat(inputs.map((v, i) =>
-        ['line', x, inputTop + i*inputGap, x + stubLen, inputTop + i*inputGap]
-    ));
+function Gate(inputs, compute) {
+    inputs = Array.isArray(inputs) ? inputs : [];
+    inputs = inputs.map(i => Object.assign(Value(i), i));
+    let len       = inputs.length;
+    let inputGap  = Math.max(spacing, Math.floor(gateSize / (len + 0)));
+    let currState = false;
+    let nextState = false;
+    let isUpdated = false;
+    return {
+        state      : () => currState,
+        inputStates: () => inputs.map(i => i.state()),
+        propagate  : () => {
+            if (isUpdated) {
+                isUpdated = false;
+                inputs.forEach(i => i.propagate());
+                let states = inputs.map(i => i.state());
+                nextState = compute(states);
+            }
+        },
+        update: () => {
+            if (!isUpdated) {
+                isUpdated = true;
+                currState = nextState;
+                inputs.forEach(i => i.update());
+            }
+        },
+        getSize: () => ({
+            w: gateSize + stubLen + 1,
+            h: Math.max(gateSize, inputGap * (len - 1) + 1)
+        }),
+        render: (x, y, not) => []
+    };
 }
 
-function renderXOr(x, y, not, inputs) {
-    let len = inputs.length;
-    let inputGap = Math.max(spacing, Math.floor(gateSize / (len + 0)));
-    let inputTop = y + Math.floor((gateSize - inputGap * (len - 1)) / 2);
-    let inputBottom = y + Math.floor((gateSize + inputGap * (len - 1)) / 2);
-    return [
-        ['path',
-            x + stubLen + 3, y,
-            [x + gateSize*3/4, y, x + gateSize, y + gateSize/2],
-            [x + gateSize*3/4, y + gateSize, x + stubLen + 3, y + gateSize],
-            [x + stubLen*2 + 3, y + gateSize*2/3, x + stubLen*2 + 3, y + gateSize/3, x + stubLen + 3, y]
-        ],
-        ['curve', x + stubLen - 1, y + gateSize, x + stubLen*2 - 1, y + gateSize*2/3, x + stubLen*2 -1, y + gateSize/3, x + stubLen - 1, y],
-        ['line', x + stubLen, inputTop, x + stubLen, inputBottom],
-        (not ? ['circle', x + gateSize + stubLen/2, y + gateSize/2, stubLen/2]
-             : ['line', x + gateSize, y + gateSize/2, x + gateSize + stubLen, y + gateSize/2]
-         )
-    ].concat(inputs.map((v, i) =>
-        ['line', x, inputTop + i*inputGap, x + stubLen, inputTop + i*inputGap]
-    ));
+function And(inputs) {
+    let gate = Gate(inputs, states => states.every(s => s));
+    let len  = gate.inputStates().length;
+    return Object.assign(gate, {
+        render: (x, y, not) => {
+            let inputGap    = Math.max(spacing, Math.floor(gateSize / (len + 0)));
+            let inputHeight = inputGap * (len - 1);
+            let topOffset   = Math.floor((gateSize - inputHeight) / 2);
+            let inputTop    = y + Math.max(0, topOffset);
+            y               = y - Math.min(0, topOffset);
+            return [
+                ['closed path',
+                    x + stubLen, y,
+                    [x + gateSize/2, y],
+                    [x + gateSize, y, x + gateSize, y + gateSize/2],
+                    [x + gateSize, y + gateSize, x + gateSize/2, y + gateSize],
+                    [x + stubLen, y + gateSize]
+                ],
+                ['line', x + stubLen, inputTop, x + stubLen, inputTop + inputHeight],
+                (not ? ['circle', x + gateSize + stubLen/2, y + gateSize/2, stubLen/2]
+                     : ['line', x + gateSize, y + gateSize/2, x + gateSize + stubLen, y + gateSize/2]
+                 )
+            ].concat(gate.inputStates().map((v, i) =>
+                ['line', x, inputTop + i*inputGap, x + stubLen, inputTop + i*inputGap]
+            ));
+        }
+    });
 }
 
-function renderNot(x, y, input) {
-    let quarter = Math.floor(gateSize/4);
-    return [
-        ['closed line',
-            x + stubLen, y,
-            x + gateSize/2, y + quarter,
-            x + stubLen, y + gateSize/2
-        ],
-        ['circle', x + gateSize/2 + stubLen/2, y + quarter, stubLen/2],
-        ['line', x, y + quarter, x + stubLen, y + quarter]
-    ];
+function Or(inputs) {
+    let gate = Gate(inputs, states => states.some(s => s));
+    let len  = gate.inputStates().length;
+    return Object.assign(gate, {
+        render: (x, y, not) => {
+            let inputGap    = Math.max(spacing, Math.floor(gateSize / (len + 0)));
+            let inputHeight = inputGap * (len - 1);
+            let topOffset   = Math.floor((gateSize - inputHeight) / 2);
+            let inputTop    = y + Math.max(0, topOffset);
+            y               = y - Math.min(0, topOffset);
+            return [
+                ['path',
+                    x + stubLen, y,
+                    [x + gateSize*3/4, y - gateSize/10, x + gateSize, y + gateSize/2],
+                    [x + gateSize*3/4, y + gateSize*11/10, x + stubLen, y + gateSize],
+                    [x + stubLen*2, y + gateSize*2/3, x + stubLen*2, y + gateSize/3, x + stubLen, y]
+                ],
+                ['line', x + stubLen, inputTop, x + stubLen, inputTop + inputHeight],
+                (not ? ['circle', x + gateSize + stubLen/2, y + gateSize/2, stubLen/2]
+                     : ['line', x + gateSize, y + gateSize/2, x + gateSize + stubLen, y + gateSize/2]
+                 )
+            ].concat(gate.inputStates().map((v, i) =>
+                ['line', x, inputTop + i*inputGap, x + stubLen, inputTop + i*inputGap]
+            ));
+        }
+    });
+}
+
+function XOr(inputs) {
+    let gate = Gate(inputs, states => states.reduce((r, s) => s ? !r : r, false));
+    let len  = gate.inputStates().length;
+    return Object.assign(gate, {
+        render: (x, y, not) => {
+            let inputGap    = Math.max(spacing, Math.floor(gateSize / (len + 0)));
+            let inputHeight = inputGap * (len - 1);
+            let topOffset   = Math.floor((gateSize - inputHeight) / 2);
+            let inputTop    = y + Math.max(0, topOffset);
+            y               = y - Math.min(0, topOffset);
+            return [
+                ['path',
+                    x + stubLen + 3, y,
+                    [x + gateSize*3/4, y, x + gateSize, y + gateSize/2],
+                    [x + gateSize*3/4, y + gateSize, x + stubLen + 3, y + gateSize],
+                    [x + stubLen*2 + 3, y + gateSize*2/3, x + stubLen*2 + 3, y + gateSize/3, x + stubLen + 3, y]
+                ],
+                ['curve', x + stubLen - 1, y + gateSize, x + stubLen*2 - 1, y + gateSize*2/3, x + stubLen*2 -1, y + gateSize/3, x + stubLen - 1, y],
+                ['line', x + stubLen, inputTop, x + stubLen, inputTop + inputHeight],
+                (not ? ['circle', x + gateSize + stubLen/2, y + gateSize/2, stubLen/2]
+                     : ['line', x + gateSize, y + gateSize/2, x + gateSize + stubLen, y + gateSize/2]
+                 )
+            ].concat(gate.inputStates().map((v, i) =>
+                ['line', x, inputTop + i*inputGap, x + stubLen, inputTop + i*inputGap]
+            ));
+        }
+    });
+}
+
+function Not(inputs) {
+    let gate = Gate(inputs, states => !!states[0]);
+    return Object.assign(gate, {
+        getSize: () => ({
+            w: gateSize/2 + stubLen + 1,
+            h: gateSize/2
+        }),
+        render: (x, y, not) => {
+            let quarter = Math.floor(gateSize/4);
+            return [
+                ['closed line',
+                    x + stubLen, y,
+                    x + gateSize/2, y + quarter,
+                    x + stubLen, y + gateSize/2
+                ],
+                ['circle', x + gateSize/2 + stubLen/2, y + quarter, stubLen/2],
+                ['line', x, y + quarter, x + stubLen, y + quarter]
+            ];
+        }
+    });
 }
 
 function renderCursor(x, y) {
@@ -108,14 +180,13 @@ function renderCursor(x, y) {
 
 function renderAll() {
     // Hard-coded entities based on mouse position, for now:
-    ui.render(
-        ...renderAnd(mouse.x, mouse.y, false, new Array(numInputs).fill(0)),
-        ...renderOr(mouse.x+50, mouse.y, false, new Array(numInputs).fill(0)),
-        ...renderXOr(mouse.x+100, mouse.y, false, new Array(numInputs).fill(0)),
-        ...renderAnd(mouse.x, mouse.y+50, true, new Array(numInputs).fill(0)),
-        ...renderOr(mouse.x+50, mouse.y+50, true, new Array(numInputs).fill(0)),
-        ...renderXOr(mouse.x+100, mouse.y+50, true, new Array(numInputs).fill(0)),
-        ...renderNot(mouse.x, mouse.y-30, 1),
-        ...renderCursor(mouse.x, mouse.y)
+    let gates = [And, Or, XOr, Not].map(g => g(new Array(numInputs).fill(0)));
+    let content = [].concat(
+        ...gates.map(g => g.getSize()).map((s, i) => [['filled #EEE rect', mouse.x + 55*i, mouse.y, s.w, s.h]]),
+        ...gates.map(g => g.getSize()).map((s, i) => [['filled #EEE rect', mouse.x + 55*i, mouse.y+55, s.w, s.h]]),
+        ...gates.map((g, i) => g.render(mouse.x + 55*i, mouse.y, false)),
+        ...gates.map((g, i) => g.render(mouse.x + 55*i, mouse.y+55, true)),
+        renderCursor(mouse.x, mouse.y)
     );
+    ui.render(...content);
 }
